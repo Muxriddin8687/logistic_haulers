828fa72308afc948e47336769566c1a641c43e40
m_satipboyev <M.Satipboyev@uzinfocom.uz>
2024-01-18 18:34:20 +0500

    fixed route

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     path: '',
     component: MainComponent,
     children: [
-      { path: 'home', component: HomeComponent },
+      { path: '', component: HomeComponent },
       { path: 'about-us', component: AboutUsComponent },
       { path: 'contact-us', component: ContactUsComponent },
       { path: 'get-quote', component: GetquoteComponent },
@@ -45,7 +45,7 @@ const routes: Routes = [
         ]
       },
 
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '', redirectTo: '', pathMatch: 'full' },
       { path: '**', redirectTo: 'home', pathMatch: 'full' }
     ]
   },
