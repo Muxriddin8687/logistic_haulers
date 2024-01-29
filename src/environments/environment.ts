f16a7ec9146e127af75625ffb7315fc491151a03
muxriddin8687 <muxriddinsatipboyev@gmail.com>
2024-01-29 22:44:40 +0500

    fix: email and confi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     prod: true,
     companyApi: 'https://api.thecompaniesapi.com/v1/companies?size=10&token=XkjLsLUY&search=',
     telegram_api: 'https://api.telegram.org/bot5201252679:AAEqE5sjhIL94ajvc04nwDJIIVR3H2m30I8/sendMessage?chat_id=@',
-    email_api: "https://api.elasticemail.com/v2/email/send?apikey=5AD98129B9CD83A847E110BEC86AD61920FEF44DC80CDBC201A747E019CCA67CC9210E7403870730121089BFD88C927C&from=info@cdbhaulers.com&fromName=CDB%20Haulers%20site&to=cdbhaulersllc@gmail.com&charset=utf-8",
+    email_api: "https://api.elasticemail.com/v2/email/send?apikey=5AD98129B9CD83A847E110BEC86AD61920FEF44DC80CDBC201A747E019CCA67CC9210E7403870730121089BFD88C927C&from=info@cdbhaulersllc.com&fromName=CDB%20Haulers%20site&to=cdbhaulersllc@gmail.com&charset=utf-8",
 };
