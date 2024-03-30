2626a78ed2ea378f0d8a29ece0e9445782ad23d6
muxriddin8687 <muxriddinsatipboyev@gmail.com>
2024-03-31 00:50:14 +0500

    update privacy police

--- src/app/main/pages/privancy/privancy.component.ts
+++ src/app/main/pages/privancy/privancy.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-privancy',
   templateUrl: './privancy.component.html',
-  styleUrls: ['./privancy.component.scss']
+  styleUrls: ['./privancy.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class PrivancyComponent {
 
--- src/app/main/pages/terms/terms.component.ts
+++ src/app/main/pages/terms/terms.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-terms',
   templateUrl: './terms.component.html',
-  styleUrls: ['./terms.component.scss']
+  styleUrls: ['./terms.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class TermsComponent {
 
